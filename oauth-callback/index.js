e72b08e1c2148eb110a06c882be7bade5cd05608
Matt Magurany <mmagurany@live.com>
2018-01-30 17:52:33 -0500

    Request token from Fitbit

--- oauth-callback/index.js
+++ oauth-callback/index.js
@@ -1,13 +1,28 @@
+var request = require('request-promise');
+var Base64 = require('js-base64');
+
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    var serviceName = req.query['service_name'];
+    var serviceName = req.query.service_name;
+    var code = req.query.code;
+    var promise;
     
-    if (serviceName === 'strava') {
-        context.log('This is an OAuth callback for Strava.');
-    }
-    else if (serviceName === 'fitbit') {
+    // if (serviceName === 'strava') {
+    //     context.log('This is an OAuth callback for Strava.');
+    // }
+    // else
+    if (serviceName === 'fitbit') {
         context.log('This is an OAuth callback for Fitbit.');
+
+        promise = requestToken('https://api.fitbit.com/oauth2/token', process.env['FITBIT_CLIENT_ID'], process.env['FITBIT_CLIENT_SECRET'], code, serviceName)
+        .then(response => {
+            return {
+                fitbitUserId: response.user_id,
+                fitbitAccessToken: response.access_token,
+                fitbitRefreshToken: response.refresh_token
+            };
+        });
     }
     else {
         context.res = {
@@ -18,12 +33,31 @@ module.exports = function (context, req) {
         return;
     }
 
-    context.res = {
-        status: 302,
-        headers: {
-            'Location': 'https://strava-to-fitbit.azurewebsites.net'
-        },
-        body: ''
-    };
-    context.done();
+    promise.then(data => {
+        context.res = {
+            status: 302,
+            headers: {
+                'Location': 'https://strava-to-fitbit.azurewebsites.net?fid=' + data.fitbitUserId
+            },
+            body: ''
+        };
+        context.done();
+    });
 };
+
+function requestToken(url, clientId, clientSecret, code, serviceName) {
+    return request({
+        method: 'POST',
+        uri: url,
+        headers: {
+            Authorization: 'Basic ' + Base64.encode(clientId + ':' + clientSecret)
+        },
+        formData: {
+            code: code,
+            grant_type: 'authorization_code',
+            client_id: clientId,
+            redirect_uri: 'https://strava-to-fitbit.azurewebsites.net/oauth/callback/' + serviceName
+        },
+        json: true
+    });
+}
