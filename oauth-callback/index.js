76b5eb0d2552d384622097839c78dd47dbf4fb87
Matt Magurany <mmagurany@live.com>
2018-01-31 22:29:13 -0500

    Move fitbit code to new file

--- oauth-callback/index.js
+++ oauth-callback/index.js
@@ -1,6 +1,5 @@
 var request = require('request-promise');
-var Base64 = require('js-base64').Base64;
-var store = require('../lib/store');
+var fitbit = require('../lib/fitbit');
 
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
@@ -12,12 +11,7 @@ module.exports = function (context, req) {
     if (serviceName === 'fitbit') {
         context.log('This is an OAuth callback for Fitbit.');
 
-        promise = requestToken('https://api.fitbit.com/oauth2/token', process.env['FITBIT_CLIENT_ID'], process.env['FITBIT_CLIENT_SECRET'], code, serviceName)
-        .then((result) => {
-            context.log('Saving tokens for Fitbit user, "' + result.user_id + '."');
-    
-            return store.addTokens(serviceName, result.user_id, result.access_token, result.refresh_token);
-        });
+        promise = fitbit.saveTokens(code);
     }
     else {
         context.res = {
@@ -48,20 +42,3 @@ module.exports = function (context, req) {
         context.done();
     });
 };
-
-function requestToken(url, clientId, clientSecret, code, serviceName) {
-    return request({
-        method: 'POST',
-        uri: url,
-        headers: {
-            'Authorization': 'Basic ' + Base64.encode(clientId + ':' + clientSecret)
-        },
-        form: {
-            code: code,
-            grant_type: 'authorization_code',
-            client_id: clientId,
-            redirect_uri: 'https://strava-to-fitbit.azurewebsites.net/oauth/callback/' + serviceName
-        },
-        json: true
-    });
-}
