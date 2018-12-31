189ca763d97aeffd601bdc39ef4a1c01b545edf4
Matt Magurany <matt.magurany@genesys.com>
2018-12-31 17:44:16 -0500

    Update authorization workflow

--- oauth-callback/index.js
+++ oauth-callback/index.js
@@ -1,11 +1,11 @@
-const request = require('request-promise');
-const fitbit = require('../lib/fitbit');
-const strava = require('../lib/strava');
 const authorization = require('../lib/authorization');
+const store = require('./store');
 
 module.exports = authorization.withUserContext((context, req) => {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    var user = context.user;
+    var userToken;
     var serviceName = req.query.service_name;
     var code = req.query.code;
     var service;
@@ -13,12 +13,22 @@ module.exports = authorization.withUserContext((context, req) => {
     if (serviceName === 'fitbit') {
         context.log('This is an OAuth callback for Fitbit.');
 
-        service = fitbit;
+        if (!user || !user.strava) {
+            context.res = {
+                status: 400,
+                body: 'Strava must be connected first.'
+            }
+            context.done();
+            return;
+        }
+
+        userToken = store.getUserToken(user.strava);
+        service = require('../lib/fitbit');
     }
     else if (serviceName === 'strava') {
         context.log('This is an OAuth callback for Strava.');
 
-        service = strava;
+        service = require('../lib/strava');
     }
     else {
         context.res = {
@@ -29,18 +39,29 @@ module.exports = authorization.withUserContext((context, req) => {
         return;
     }
 
-    service.saveAuthorization(code)
-    .then((userId) => {
-        var user = context.user;
+    service.fromAuthorizationCode(code)
+    .then((result) => {
+
+        if (!userToken) {
+            userToken = {
+                [serviceName]: {
+                    userId: result.userId,
+                    accessToken: result.accessToken,
+                    refreshToken: result.refreshToken
+                }
+            };
+        }
+        store.saveUserToken(userToken);
         
         if (!user) {
             user = {};
         }
         
-        user[serviceName] = userId;
+        user[serviceName] = result.userId;
         context.res = {
             status: 302,
             headers: {
+                // TODO: get the redirect location from the state param
                 'Location': 'https://strava-to-fitbit.azurewebsites.net'
             },
             body: ''
