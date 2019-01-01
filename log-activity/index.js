8549c316f7bd73ef740ecf098bb4907e6a0d2b76
Matt Magurany <matt.magurany@genesys.com>
2019-01-01 17:08:06 -0500

    Add code to log an activity to Fitbit

--- log-activity/index.js
+++ log-activity/index.js
@@ -1,5 +1,6 @@
 const store = require('../lib/store');
 const strava = require('../lib/strava');
+const fitbit = require('../lib/fitbit');
 
 module.exports = function (context) {
     const event = context.bindings.stravaEvent;
@@ -10,11 +11,20 @@ module.exports = function (context) {
         context.log('Fetched user token');
         
         const stravaApi = strava.fromUser(user);
+        const fitbitApi = fitbit.fromUser(user);
 
         context.log('Strava API: ' + stravaApi);
         
         return stravaApi.getActivity(event.activityId).then((stravaActivity) => {
             context.log('Fetched strava activity.\n' + JSON.stringify(stravaActivity));
+
+            // TODO: Check if activity was already logged
+            // TODO: Map Strava activity types to Fitbit activity types
+            // TODO: Log activity
+            // return fitbitApi.logActivity({
+                
+            // })
+            // TODO: Save activity IDs to storage
         });
     }).catch((error) => {
         context.log('Failed to log activity to Fitbit.\n' + error);
